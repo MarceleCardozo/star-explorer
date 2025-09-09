ca0eca0bfef53b5bbd6cbeeb8d677dc088db4515
MarceleCardozo <marcelecardozo@gmail.com>
2025-09-09 17:58:00 -0300

    chore: update gitignore and disable coverage collection
    - Add coverage/ to gitignore to exclude coverage reports
    - Set collectCoverage to false in jest.config.js to disable coverage collection
    - Remove coverage report files that were previously generated

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect', '<rootDir>/jest.setup.js'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: [
     '**/*.{js,jsx,ts,tsx}',
     '!**/coverage/**',
